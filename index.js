a936e25ae91d06ba0a132189a1648173cb8d377d
Amirkhan Orazbay <190107066@stu.sdu.edu.kz>
2022-10-23 06:21:11 +0600

    - add match start btn - current state of match flags (routes) - add new script to match web page

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const port = 80
 
 let team_number;
 let team_names;
+let current_match_id = -1;
+let current_match_score = [0,0];
+let current_match_team_name = [];
 let matches = [];
+let totalSeconds = 0;
+let timerVariable;
 
 app.use(express.static('./'))
 app.use(bodyParser.urlencoded({
@@ -27,17 +32,54 @@ app.post('/team_number', (req, res) => {
 })
 
 app.get('/team_names', (req, res) => {
-    res.send(matches);
+    res.send(matches)
 })
 
 app.post('/team_names', (req, res) => {
-    team_names = req.body;
-    const matches_number = team_number / 2;
+    team_names = req.body
+    const matches_number = team_number / 2
     for (let i = 1; i <= matches_number; i++) {
-        console.log(i);
         matches[i-1] = [team_names["team_name" + (2 * i - 1)], team_names["team_name" + 2 * i]]
     }
     res.redirect("/form_matches.html");
+})
+
+app.get('/match/:ID', (req, res) => {
+    current_match_id = req.params.ID
+    current_match_team_name = matches[+current_match_id - 1]
+    res.redirect("/match.html")
+    timerVariable = setInterval(() => totalSeconds++, 1000);
+})
+
+app.get('/match/:TEAM_ID/score', (req, res) => {
+    const TEAM_ID = req.params.TEAM_ID;
+    res.send(current_match_score[+TEAM_ID - 1] + "")
+})
+
+app.get('/match/:TEAM_ID/score/add', (req, res) => {
+    const TEAM_ID = req.params.TEAM_ID;
+    current_match_score[+TEAM_ID - 1] = current_match_score[+TEAM_ID - 1] + 1
+    res.send(current_match_score[+TEAM_ID - 1] + "")
+})
+
+app.get('/match/:TEAM_ID/score/sub', (req, res) => {
+    const TEAM_ID = req.params.TEAM_ID;
+    current_match_score[+TEAM_ID - 1] = current_match_score[+TEAM_ID - 1] - 1
+    res.send(current_match_score[+TEAM_ID - 1] + "")
+})
+
+
+app.get('/match/:TEAM_ID/name', (req, res) => {
+    const TEAM_ID = req.params.TEAM_ID;
+    res.send(current_match_team_name[+TEAM_ID - 1])
+})
+
+app.get('/time', (req, res) => {
+    res.send(totalSeconds + "");
+})
+
+app.get('/stop', (req, res) => {
+    res.send(totalSeconds + "");
 })
 
 app.listen(port, () => {
